8bc4e7e2da5ba0df4037b68d487c4432810c5001
wanny <choijw1116@gmail.com>
2024-06-06 21:26:44 +0900

    [Fix] Fix variables and getter, setter

--- src/main/java/member/request/PostMemberReq.java
+++ src/main/java/member/request/PostMemberReq.java
@@ -1,4 +1,64 @@
 package member.request;
 
 public class PostMemberReq {
+    String email;
+    String password;
+    String nickname;
+    String talent;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getTalent() {
+        return talent;
+    }
+
+    public void setTalent(String talent) {
+        this.talent = talent;
+    }
+    //    public String getId() {
+//        return email;
+//    }
+//
+//    public void setId(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getPw() {
+//        return password;
+//    }
+//
+//    public void setPw(String password) {
+//        this.password = password;
+//    }
+//
+//
+//    public String getNickname() {
+//        return nickname;
+//    }
+//
+//    public void setName(String nickname) {
+//        this.nickname = nickname;
+//    }
 }
